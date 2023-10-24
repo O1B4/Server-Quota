66dc00a14b9d4e7f67db027576dc26aff63a371e
DESKTOP-KTEEVGU\hanme <2hanme527@naver.com>
2023-10-24 16:27:53 +0900

    [chore] BeanConfig 추가 modelMapper 사용을 위한 beanConfig 추가

---
+++ server-quota/src/main/java/com/o1b4/serverquota/configuration/BeanConfig.java
@@ -0,0 +1,13 @@
+package com.o1b4.serverquota.configuration;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class BeanConfig {
+    @Bean
+    public ModelMapper modelMapper() {
+        return new ModelMapper();
+    }
+}
