410661a13c0ded1f024f244e2e56c72d966eafca
DESKTOP-KTEEVGU\hanme <2hanme527@naver.com>
2023-10-26 06:10:50 +0900

    [chore] 사용하지 않는 import 삭제

--- server-quota/src/main/java/com/o1b4/serverquota/repository/TeamRepository.java
+++ server-quota/src/main/java/com/o1b4/serverquota/repository/TeamRepository.java
@@ -2,7 +2,6 @@ package com.o1b4.serverquota.repository;
 
 import com.o1b4.serverquota.entity.Team;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 public interface TeamRepository extends JpaRepository<Team, Long> {
