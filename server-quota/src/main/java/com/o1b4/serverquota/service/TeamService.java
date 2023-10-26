8f0c5abaaaa74e87ee65d4fbf104eb968e999729
DESKTOP-KTEEVGU\hanme <2hanme527@naver.com>
2023-10-27 03:11:35 +0900

    [feat] teamID로 팀원 이름, 프로필 사진 모두 조회

--- server-quota/src/main/java/com/o1b4/serverquota/service/TeamService.java
+++ server-quota/src/main/java/com/o1b4/serverquota/service/TeamService.java
@@ -1,22 +1,28 @@
 package com.o1b4.serverquota.service;
 
 import com.o1b4.serverquota.dto.response.MainTeamDTO;
+import com.o1b4.serverquota.dto.response.TeamMemberDTO;
 import com.o1b4.serverquota.entity.BelongTeam;
 import com.o1b4.serverquota.entity.Team;
 import com.o1b4.serverquota.exception.CustomApiException;
 import com.o1b4.serverquota.repository.BelongTeamRepository;
 import com.o1b4.serverquota.repository.TeamRepository;
+import com.o1b4.serverquota.repository.UserRepository;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class TeamService {
     private final TeamRepository teamRepository;
-
+    private final UserRepository userRepository;
     private final BelongTeamRepository belongTeamRepository;
 
-    public TeamService(TeamRepository teamRepository, BelongTeamRepository belongTeamRepository) {
+    public TeamService(TeamRepository teamRepository, UserRepository userRepository, BelongTeamRepository belongTeamRepository) {
         this.teamRepository = teamRepository;
+        this.userRepository = userRepository;
         this.belongTeamRepository = belongTeamRepository;
     }
 
@@ -34,5 +40,29 @@ public class TeamService {
                 .teamUrl(team.getTeamUrl())
                 .role(String.valueOf(belongTeam.getUserRole()))
                 .build();
+    }
+
+    public List<TeamMemberDTO> findTeamMembersByTeamId(long teamId) {
+
+        // teamId를 받아 belongTeam 매핑 테이블 조회
+        List<BelongTeam> belongTeams = belongTeamRepository.findByTeamId(teamId);
+
+        // list에 해당하는 room들 DTO 매핑
+        return belongTeams.stream()
+                .map(
+                        belongTeam -> TeamMemberDTO.builder()
+                        .userName(
+                                userRepository.findUserByUserId(belongTeam.getUserId())
+                                .orElseThrow(() -> new CustomApiException(HttpStatus.NOT_FOUND, "회원을 조회할 수 없습니다."))
+                                .getUserName()
+                        )
+                        .userProfileImage(
+                                userRepository.findUserByUserId(belongTeam.getUserId())
+                                .orElseThrow(() -> new CustomApiException(HttpStatus.NOT_FOUND, "회원을 조회할 수 없습니다."))
+                                .getUserProfileImage()
+                        )
+                        .build()
+                )
+                .collect(Collectors.toList());
     }
 }
